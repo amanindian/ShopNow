4f294f0c6f967efc08b9858ea05a063c1f9b3bf5
Aman Kumar <101081368+amanindian@users.noreply.github.com>
2023-06-13 13:54:20 +0530

    Fix Rating Icon

--- src/Components/Main/Product.jsx
+++ src/Components/Main/Product.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
 export default function Product({
   index,
   element,
   HendleAddToCart,
   ShowProduct,
 }) {
+  const { Image, Title, Amount, Description } = element;
+
   return (
     <div className="pro" key={index}>
       <Link to="/SingleProduct" onClick={ShowProduct}>
-        <img className="ProClickImg" src={element.Image} alt="/" />
+        <img className="ProClickImg" src={Image} alt="/" />
       </Link>
       <div className="des">
         <span>adidas</span>
-        <h5 className="about">{element.Title}</h5>
+        <h5 className="about">{Title}</h5>
         <div className="star">
-          <i className="fas fa-star" />
-          <i className="fas fa-star" />
-          <i className="fas fa-star" />
-          <i className="fas fa-star" />
-          <i className="fas fa-star" />
+          {[...Array(5)].map((_, i) => (
+            <FontAwesomeIcon key={i} icon={faStar} />
+          ))}
         </div>
-        <h4 className="proprise">{element.Amount}</h4>
+        <h4 className="proprise">{Amount}</h4>
       </div>
       <i className="fa-solid fa-cart-arrow-down" onClick={HendleAddToCart} />
       <div className="proDesc" style={{ display: "none" }}>
-        {element.Description}
+        {Description}
       </div>
     </div>
   );
